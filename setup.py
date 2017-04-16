50d8039f0f1ad227edfa0e93f44e4c6e22066a7e
Pip Jones <code@scipilot.org>
2017-04-16 18:54:14 +1000

    Added setup.py to make this installable with pip

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='fretboard',
+    version='1.0.0',
+    description='Python project that shows scales and chords on an ASCII fretboard for guitar or bass',
+    long_description=readme,
+    author='Steven Muschalik',
+    author_email='steven.muschalik@gmail.com',
+    url='https://github.com/fusionprogguy/Fretboard',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
